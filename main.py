fd103b9cc0c5f7d12fa3950b13f74590d5685f77
Christian Krause <122640925+avreo1a@users.noreply.github.com>
2024-06-29 17:53:10 -0400

    added

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ import os
 gpus = tf.config.experimental.list_physical_devices('GPU')
 if gpus:
     print("Detected GPUs:")
+else:
+    print("No Detectable GPU's")
         
 for gpu in gpus:
     tf.config.experimental.set_memory_growth(gpu, True)
